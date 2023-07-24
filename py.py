853a25342c90c8bd19889fd2098edc54cc9091a5
Ketan More <moreketan2002@gmail.com>
2023-07-24 18:04:50 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import time
 
 def get_ipv4_address():
     try:
@@ -8,14 +9,24 @@ def get_ipv4_address():
         print("IPv4 Address:", ipv4_address)  # Debugging statement
         return ipv4_address
     except socket.gaierror:
-        return '127.0.0.1'  # Use localhost as the default IP address
+        return None  # Return None if the IPv4 address is not available
+
+def start_server(ipv4_address):
+    os.system(f"cd C:\\web-logger-main && python manage.py runserver {ipv4_address}:8000")
 
 def main():
-    ipv4_address = get_ipv4_address()
-    os.system(f"cd C:\\web-logger-main && python manage.py runserver {ipv4_address}:8000")
-    
+    while True:
+        ipv4_address = get_ipv4_address()
+        if ipv4_address and ipv4_address != "127.0.0.1":
+            print("Network connection established.")
+            start_server(ipv4_address)
+            break  # Break the loop if the IPv4 address is available and not localhost
+
+        print("Waiting for network connection...")
+        time.sleep(5)  # Wait for 5 seconds before checking again
+
     # Add this line to keep the command prompt window open until a key is pressed
-    input("Press Enter any key to close...")
+    input("Press Enter to close...")
 
 if __name__ == "__main__":
     main()
